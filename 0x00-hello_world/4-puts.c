222131bb6b30324c32e1a80bc08103de8b18ac79
chaymae lgh <laghzalchaymae3@gmail.com>
2023-03-16 18:52:20 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
 /**
-*main - entry poit
-*return: always 0 (success)
+*main - prints "programming is like building a multilingual
+*puzzle, followed by a newline.
+*Return: always 0.
 */
+
+
 int main(void)
+
+
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+       puts("\"programming is like building a multilingual puzzle");
+       return (0);
 }
