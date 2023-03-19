3b13f940c1e0e1a41454e88961e9576c6ff37015
chaymae lgh <laghzalchaymae3@gmail.com>
2023-03-18 20:20:07 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int d;
+	int x;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(x);
+	putchar('\n');
+
+	return (0);
+
+}
