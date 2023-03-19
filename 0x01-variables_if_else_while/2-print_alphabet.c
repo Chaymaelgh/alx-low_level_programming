fbebf7c61bd1bf3f662b985a1c16cc09a74b3503
chaymae lgh <laghzalchaymae3@gmail.com>
2023-03-18 17:57:36 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabetic
+ * Reurn: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
