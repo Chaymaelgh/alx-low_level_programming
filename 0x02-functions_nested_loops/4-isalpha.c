5d9372b1e44176c33332a433382d49af4ab0299a
chaymae lgh <laghzalchaymae3@gmail.com>
2023-03-22 17:37:21 -0700

    task5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include"main.h"
 /*
- * _isalpha - function that checks for alphabetic character
+ * _isalpha - function that cheks for alphabetic character
  * @c: the character to be checked
  * Return: Always (Success)
+ * Description: function that checks for alphabetic character
  *
  */
 int _isalpha(int c)
